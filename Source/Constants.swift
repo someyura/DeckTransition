56f65802ccc4683d7b96110855b2b676896adf16
Yury Imashev <imashev@protonmail.com>
2018-05-25 01:59:32 +0300

    Update UI constants

--- Source/Constants.swift
+++ Source/Constants.swift
@@ -10,18 +10,18 @@ struct Constants {
     
     /// Default duration for present and dismiss animations when the user hasn't
     /// specified one
-    static let defaultAnimationDuration: TimeInterval = 0.3
+    static let defaultAnimationDuration: TimeInterval = 0.5
     
     /// The corner radius applied to the presenting and presented view
     /// controllers's views
-    static let cornerRadius: CGFloat = 8
+    static let cornerRadius: CGFloat = 10
     
     /// The alpha value of the presented view controller's view
-    static let alphaForPresentingView: CGFloat = 0.8
+    static let alphaForPresentingView: CGFloat = 0.5
     
     /// As best as I can tell using my iPhone and a bunch of iOS UI templates I
     /// came across online, 8 points is the distance between the top edges of
     /// the presented and the presenting views
-    static let insetForPresentedView: CGFloat = 8
+    static let insetForPresentedView: CGFloat = 12
     
 }
